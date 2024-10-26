ba7d51b3440157d4a0e1b9a314fe5c68090815d4
siggraphbnmit <119809082+siggraphbnmit@users.noreply.github.com>
2024-10-26 10:19:51 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -32,13 +32,13 @@ const loadingMessages = [
 ];
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyBR08BDsS3ny7XQ-N1mezON22wta6QaM3k",
+  authDomain: "sigg-waitlist-2024.firebaseapp.com",
+  databaseURL: "https://sigg-waitlist-2024-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sigg-waitlist-2024",
+  storageBucket: "sigg-waitlist-2024.appspot.com",
+  messagingSenderId: "211872816380",
+  appId: "1:211872816380:web:e626cd629430cc676a4b79"
 };
 
 let app;
